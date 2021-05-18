34894b6aeecc4fcd3dfbd93641816a82033aa525
hsj <seungjune.hwang@gmail.com>
2021-05-18 16:52:48 +0900

    리퀘스트 폼 데이터 전달 추가

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,7 +20,17 @@ def hello():
 
 @app.route('/naver')
 def naver():
-    return '네이버 페이지'
+    return render_template("naver.html")
+
+@app.route('/gonaver', methods=['GET', 'POST'])
+def gonaver():
+    if request.method == 'GET':
+        return "데이터를 받아주는 페이지"
+    else:
+        # 여기 POST로 들어오는 데이터를 받아보자
+        search = request.form['fname']
+        print("전달된값:", search)
+        return '당신이 검색한 키워드(POST)<br>{}입니다'.format(search)
 
 
 if __name__ == '__main__':
