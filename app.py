15425e598440ff30859b61d46896103bfd128efb
hsj <seungjune.hwang@gmail.com>
2021-05-25 17:18:01 +0900

    로그인 회원가입

--- app.py
+++ app.py
@@ -3,20 +3,45 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '''
-    <!DOCTYPE html>
-    <html>
-    <body>
-    <h1>웹앱프로그래밍</h1>
-    <p><a href="http://127.0.0.1:5000/hello">헬로 페이지</a></p>
-    <p><a href="http://127.0.0.1:5000/naver">네이버 페이지</a></p>
-    </body>
-    </html>
-    '''
+    return render_template("main.html")
 
 @app.route('/hello')
 def hello():
     return render_template("main.html")
+
+@app.route('/signin')
+def signin():
+    return render_template("signin.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        # 여기 POST로 들어오는 데이터를 받아보자
+        email = request.form['email']
+        pwd = request.form['pwd']
+        print("전달된값:", email, pwd)
+        # 만약에 이메일과 패스워드 같다면
+        if email == 'a@a.com' and pwd == '1234':
+        # 로그인 성공
+            return "로그인 성공"
+        # 아니면
+        else:
+        # 아이디 패스워드 확인
+            return "아이디 패스워드 확인"
+    
+
+@app.route('/action_page', methods=['GET', 'POST'])
+def action_page():
+    if request.method == 'GET':
+        return "데이터를 받아주는 페이지"
+    else:
+        # 여기 POST로 들어오는 데이터를 받아보자
+        email = request.form['email']
+        pwd = request.form['pwd']
+        print("전달된값:", email, pwd)
+        return '회원가입 데이터(POST)'
 
 @app.route('/naver')
 def naver():
